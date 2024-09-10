52b4badcff2edf98666e7de5047b555328ce86c4
YU <111847207+YU000jp@users.noreply.github.com>
2024-09-10 22:55:44 +0900

    feat(): logseq-l10n (translate)

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,37 @@
 import '@logseq/libs' //https://plugins-doc.logseq.com/
-import { setup as l10nSetup } from "logseq-l10n" //https://github.com/sethyuan/logseq-l10n
-import ja from "./translations/ja.json"
 import { BlockEntity } from '@logseq/libs/dist/LSPlugin.user'
+import { setup as l10nSetup, t } from "logseq-l10n" //https://github.com/sethyuan/logseq-l10n
 import { getTitleFromURL } from './getTitle'
-import { t } from 'logseq-l10n'
+import { settingsTemplate } from './settings'
+import af from "./translations/af.json"
+import de from "./translations/de.json"
+import es from "./translations/es.json"
+import fr from "./translations/fr.json"
+import id from "./translations/id.json"
+import it from "./translations/it.json"
+import ja from "./translations/ja.json"
+import ko from "./translations/ko.json"
+import nbNO from "./translations/nb-NO.json"
+import nl from "./translations/nl.json"
+import pl from "./translations/pl.json"
+import ptBR from "./translations/pt-BR.json"
+import ptPT from "./translations/pt-PT.json"
+import ru from "./translations/ru.json"
+import sk from "./translations/sk.json"
+import tr from "./translations/tr.json"
+import uk from "./translations/uk.json"
+import zhCN from "./translations/zh-CN.json"
+import zhHant from "./translations/zh-Hant.json"
 
 
 /* main */
 const main = async () => {
 
-    await l10nSetup({ builtinTranslations: { ja } })
+    await l10nSetup({
+        builtinTranslations: {//Full translations
+            ja, af, de, es, fr, id, it, ko, "nb-NO": nbNO, nl, pl, "pt-BR": ptBR, "pt-PT": ptPT, ru, sk, tr, uk, "zh-CN": zhCN, "zh-Hant": zhHant
+        }
+    })
 
     /* user settings */
     logseq.useSettingsSchema(settingsTemplate())
